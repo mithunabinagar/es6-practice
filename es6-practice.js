b0ef3815b7bb5ddb8af0ba87caac7b8ef7eb59d5
mithunabinagar <mithunabinagar@gmail.com>
2020-08-05 14:34:27 -0700

    es6

---
+++ es6-practice.js
@@ -0,0 +1,17 @@
+const hubby = "Elias Kanchon";
+// hubby = "Omar Sunny";
+console.log(hubby);
+
+const numbers = [12, 45,22];
+numbers[1] = 88;
+numbers.push(12);
+numbers.pop();
+const nayok = {name:"Sakib Khan", phone:5462};
+// console.log(numbers);
+
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khala";
+console.log(patientName);
+
+
+
