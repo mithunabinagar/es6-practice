d6bf15df7aaaef07a831bb9534fd5f5b22892034
mithunabinagar <mithunabinagar@gmail.com>
2020-08-05 15:40:58 -0700

    23-4

---
+++ 23-4.js
@@ -0,0 +1,17 @@
+const firstName = "Azizul";
+const lastName  = "Haque";
+// const fullName = firstName + lastName;
+// const fullName = firstName + " " + lastName + " " + "is a good boy";
+const fullName2 = `${firstName} ${lastName} is a good boy`;
+
+const multiLine = "I love you\n"
+                + "I miss you\n"
+                + "I need you"
+
+ const multiLine2 = `I love you
+I miss you
+I need you`
+               
+
+
+console.log(multiLine2);
