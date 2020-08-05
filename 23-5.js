d69ce245879c8bc8816b25d571b604747cc23e06
mithunabinagar <mithunabinagar@gmail.com>
2020-08-05 16:20:27 -0700

    23-5

---
+++ 23-5.js
@@ -0,0 +1,20 @@
+// function doubleIt(num){
+//     return num * 2;
+// }
+
+
+// const doubleIt = function myFun(num){
+//     return * 2;
+// }
+
+// 
+
+
+const bigMath = (x, y) =>{
+    const sum = x + y;
+    const diff = x - y;
+    const result = sum * diff;
+    return result;
+}
+const result3 = bigMath(12, 8);
+console.log(result3);
